91ba9d7d15d28b94cd0048d50000a270e85106d7
= <=>
2019-07-24 17:22:39 +0300

    added landlord register endpoint

--- rms-api/endpoints/users-endpoints/landlord-endpoints/register.php
+++ rms-api/endpoints/users-endpoints/landlord-endpoints/register.php
@@ -1,13 +1,19 @@
 <?php 
 
-    require_once "../../database.php";
-    require_once "../../data/user/Landlord.php";
+    require_once "../../../database.php";
+    require_once "../../../data/user/Landlord.php";
 
     $dbinst = new Database;
     $conn = $dbinst->getConn();
     $user = new LandLord($conn);
 
-    $user->id = 1;
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // $user->id = 1;
 
     // $user->currentPassw = "kelvin";
 
@@ -16,12 +22,17 @@
     // $user->gender = "male";
     // $user->nationalId = 1122678;
     // $user->phoneNo = "0727456354";
-    // $user->email = "kelraf@gmail.com";
-    // $user->passw = "kelraf";
-    // $user->confirmPassw = "kelraf";
+    $user->email = $data["email"];
+    $user->passw = $data["passw"];
+    $user->confirmPassw = $data["confPassw"];
 
-    // print_r($user->register());
-    print_r($user->updatePasswords());
+    $done = $user->register();
+    if($done["bool"]) {
+        echo json_encode($done);
+    } else {
+        echo json_encode($done);
+    }
+    // print_r($user->updatePasswords());
     // print_r($user->getUser());
     // print_r($user->updateInfor());
     // print_r($user->deleteUser());
