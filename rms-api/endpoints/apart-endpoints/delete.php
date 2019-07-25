460739d0d32468a57622d93dd848c29d2bd4a568
= <=>
2019-07-25 21:12:32 +0300

    added all the apartment endpoints

--- rms-api/endpoints/apart-endpoints/delete.php
+++ rms-api/endpoints/apart-endpoints/delete.php
@@ -7,16 +7,14 @@
     $apartment = new Apartment($dbinst);
     
     header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Methods: DELETE");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
     $data = json_decode(file_get_contents("php://input"), true);
 
-    $apartment->apartment_name = $data["apartment_name"];
-    $apartment->apartment_location = $data["apartment_location"];
-    $apartment->landlord_id = $data["landlord_id"];
+    $apartment->id = $data["id"];
 
-    $done = $apartment->addApart();
+    $done = $apartment->deleteApart();
 
     echo json_encode($done);
 
