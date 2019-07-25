fe8ee79e9818429050e730a522b3ef7a9b029696
= <=>
2019-07-25 20:41:17 +0300

    restructured database connection in apartment model and apartment create endpoint

--- rms-api/endpoints/apart-endpoints/create.php
+++ rms-api/endpoints/apart-endpoints/create.php
@@ -1,19 +1,10 @@
 <?php 
 
     require_once "../../database.php";
+    require_once "../../data/apartments/apartment.php";
 
     $apartmentName = $_POST["apartmentName"];
     $apartmentLocation = $_POST["apartmentLocation"];
     $landlordId = 1; 
 
-    $done = mysqli_query($conn, 
-                        "INSERT INTO apartments (apartmentName, apartmentLocation, landlordId)
-                            VALUES('$apartmentName', '$apartmentLocation', '$landlordId')"
-                        );
-
-    if($done) {
-        header("Location: ../../../rms-ui/pages/dashboard.php");
-    } else {
-        echo "Error";
-        header("Location: ../../../rms-ui/pages/apartment_form.php");
-    }
+   
