2fd2d4753aac48697cf051dd3c087066db107a6f
= <=>
2019-07-27 19:46:13 +0300

    cleanup

--- rms-api/endpoints/house-endpoints/create.php
+++ rms-api/endpoints/house-endpoints/create.php
@@ -1,25 +1,9 @@
 <?php
 
     require_once "../../database.php";
-
-    $houseType = $_POST["houseType"];
-    $status = $_POST["status"];
-    $rent = $_POST["rent"];
-
-    $landlordId = 1;
-    $apartmentId = 2;
-
-    $done = mysqli_query(
-        $conn,
-        "INSERT INTO houses (houseType, apartmentId, landlordId, status, rent)
-         VALUES('$houseType', '$apartmentId', '$landlordId', '$status', '$rent')"
-    );
-
-    if ($done) {
-        header("Location: ../../../rms-ui/pages/dashboard.php");
-    } else {
-        echo "Error".mysqli_error($conn);
-        // header("Location: ../../../rms-ui/pages/house_form.php");
-    }
+    require_once "../../data/houses/houses.php";
+    
+    $dbinst = new Database;
+    $house = new House($dbinst);
 
 ?>
