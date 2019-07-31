ac747327144381c749f070ce4dc5dba425ce5224
= <=>
2019-07-31 20:33:58 +0300

    added Login

--- rms-api/endpoints/auth/login.php
+++ rms-api/endpoints/auth/login.php
@@ -1,5 +1,8 @@
 <?php
 
+    require_once "../../database.php";
+    require_once "../../data/user/Landlord.php";
+
     class Login {
 
         private $landlord;
@@ -14,28 +17,23 @@
 
         }
 
-        // public function verify_password() {
-        //     $this->password = filter_var($this->password, FILTER_SANITIZE_STRING);
-        //     if(password_verify($this->password)) {
-
-        //     }
-        // }
-
         public function login() {
+            $this->landlord->email = $this->email;
             $user = $this->landlord->vEmail(true);
             if(!$user["bool"]) {
                 return $user;
             } else {
                 $this->password = filter_var($this->password, FILTER_SANITIZE_STRING);
-                if(password_verify($user["passw"], $this->password)) {
-
+                print_r($user);
+                if(password_verify($this->password, $user["data"]["passw"])) {
+                    echo $user["data"]["passw"];
                     $data = [];
-                    $data["user_id"] = $user["id"];
-                    $data["user_role"] = $user["role"];
-                    $data["email"] = $user["email"];
+                    $data["user_id"] = $user["data"]["id"];
+                    $data["user_role"] = $user["data"]["role"];
+                    $data["email"] = $user["data"]["email"];
 
                     return ["bool" => true, "data" => $data];
-                    
+
                 } else {
                     return ["bool" => false, "message" => "Invalid Password"];
                 }
@@ -46,3 +44,11 @@
 
         }
     }
+
+    $db = new Database;
+    $conn = $db->getConn();
+    $landlord = new LandLord($conn);
+
+    $login = new Login($landlord, "wangui@gmail.com", "wangui");
+
+    print_r($login->login());
